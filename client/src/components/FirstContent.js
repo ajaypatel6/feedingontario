cdfda68dc34330a5d3eee7425ff08c3930900742
Ajay <ajaypatel23623@gmail.com>
2021-12-31 22:35:15 -0500

    added navbar, prob change, content overlapping

---
+++ client/src/components/FirstContent.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const FirstContent = () => {
+  return (
+    <div className="firstContent">
+      <div>
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada
+          erat eget efficitur auctor. Morbi vel laoreet mi. Vivamus sed ornare
+          felis, in blandit augue. Sed ultricies, risus ut feugiat gravida,
+          dolor quam mattis ex, hendrerit sodales velit odio vel dolor. Nulla
+          nec blandit est. Pellentesque quis leo leo. Nunc sed ligula a turpis
+          malesuada gravida nec ac turpis. Suspendisse volutpat rutrum tellus.
+          Cras gravida est lorem, vitae scelerisque mauris laoreet non. Mauris
+          velit massa, laoreet et varius et, porta vitae nulla. Suspendisse
+          vehicula turpis nec blandit posuere. In tellus tellus, posuere in
+          ultrices eu, consequat sit amet nisi. Donec justo dui, porta at magna
+          condimentum, sollicitudin malesuada orci. Nam sit amet lectus posuere,
+          dictum neque id, interdum tortor. Maecenas et commodo nulla.
+          Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+          posuere cubilia curae; In interdum commodo risus a congue. Nulla
+          semper ante eget augue rhoncus malesuada. Vestibulum ut tempus tellus,
+          vel sollicitudin odio. Aliquam erat volutpat. Morbi viverra rhoncus
+          varius. Nunc et mollis nisi, eget egestas turpis. Proin vel ante
+          posuere, sodales purus vitae, fermentum ante. Phasellus vel ipsum at
+          erat congue egestas rutrum molestie metus. In volutpat placerat
+          faucibus. Pellentesque finibus, mi vitae laoreet convallis, mauris leo
+          dictum purus, et faucibus enim magna ut nulla. Cras luctus magna a
+          arcu aliquam, in rhoncus sem facilisis. Donec sed scelerisque augue,
+          nec vulputate mi. Ut rhoncus molestie ipsum nec elementum. Aliquam non
+          eros quis est pulvinar cursus. Donec ullamcorper, libero ut sagittis
+          dictum, felis nisl pretium odio, ac dictum libero velit nec velit. In
+          a viverra eros. Integer tincidunt nisi finibus purus dignissim
+          iaculis. Sed ut tortor mattis, hendrerit nisl vitae, cursus elit.
+          Etiam feugiat eu arcu et sollicitudin. In condimentum ipsum vel diam
+          lobortis, vel venenatis augue vehicula. Sed ornare, risus a viverra
+          facilisis, nulla turpis tincidunt dolor, vitae suscipit ligula eros
+          vel enim. In hac habitasse platea dictumst. Donec nunc nunc, auctor et
+          auctor a, pharetra fringilla ante. Quisque consectetur tortor nisl,
+          non dapibus justo volutpat vel. Nam ac ultrices felis. Nullam dolor
+          mi, pharetra id massa id, dignissim luctus felis. Fusce elementum
+          libero quis est euismod maximus. Curabitur efficitur magna et urna
+          pellentesque, vitae imperdiet tellus dignissim. Nulla vel elit sit
+          amet tellus ultricies ultricies. In hac habitasse platea dictumst.
+          Pellentesque enim nunc, semper eget commodo sit amet, auctor et purus.
+          Donec varius erat dignissim lectus maximus venenatis. Nullam
+          condimentum mattis laoreet. Vestibulum non augue rhoncus, lacinia enim
+          vitae, consequat arcu. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. Cras scelerisque ultricies
+          pellentesque.
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default FirstContent;
